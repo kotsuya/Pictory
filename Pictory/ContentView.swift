ffac8a485a9c9eae69969f98e2bacead84d83505
Yoo <kotsuya00@gmail.com>
2024-11-04 09:02:45 +0900

    Initial Commit

---
+++ Pictory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pictory
+//
+//  Created by YOO on 2024/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pictory/PictoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictoryApp.swift
+//  Pictory
+//
+//  Created by YOO on 2024/11/04.
+//
+
+import SwiftUI
+
+@main
+struct PictoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
